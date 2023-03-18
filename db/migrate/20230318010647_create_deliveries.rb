60e9fd284aa603d1edbff33a13d8baa5a049febd
msmithivas <msmithivas@gmail.com>
2023-03-18 01:11:20 +0000

    Initial commit Created database tables

---
+++ db/migrate/20230318010647_create_deliveries.rb
@@ -0,0 +1,15 @@
+class CreateDeliveries < ActiveRecord::Migration[6.0]
+  def change
+    create_table :deliveries do |t|
+      t.string :description
+      t.date :arrival_date
+      t.string :carrier
+      t.string :tracking_number
+      t.integer :user_id
+      t.string :status
+      t.string :details
+
+      t.timestamps
+    end
+  end
+end
